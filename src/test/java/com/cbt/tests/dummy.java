dfddbef3c5fb7250b712384cd201201c7e0bccf1
Alperen <80896036+Alpertek@users.noreply.github.com>
2021-06-12 01:08:30 +0300

    Update dummy.java

--- src/test/java/com/cbt/tests/dummy.java
+++ src/test/java/com/cbt/tests/dummy.java
@@ -3,5 +3,7 @@ package com.cbt.tests;
 public class dummy {
     public static void main(String[] args) {
         System.out.println("Dummy talks!!");
+        System.out.println("Dummy shuts up!!");
+        
     }
 }
