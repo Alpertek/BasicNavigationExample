ee22aad2d577b795dfa20bc047abed84f5498c4b
Alperen <80896036+Alpertek@users.noreply.github.com>
2021-06-12 01:09:41 +0300

    Update dummy.java

--- src/test/java/com/cbt/tests/dummy.java
+++ src/test/java/com/cbt/tests/dummy.java
@@ -4,6 +4,7 @@ public class dummy {
     public static void main(String[] args) {
         System.out.println("Dummy talks!!");
         System.out.println("Dummy shuts up!!");
+        System.out.println("this is fetched from github");
         
     }
 }
