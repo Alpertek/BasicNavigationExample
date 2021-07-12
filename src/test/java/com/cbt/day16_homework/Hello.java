d158e3189c41664d41694178668a9372c2c8c22f
anonimid <freelancetradertr@gmail.com>
2021-07-12 19:09:47 +0300

    Changed exception kind

--- src/test/java/com/cbt/day16_homework/Hello.java
+++ src/test/java/com/cbt/day16_homework/Hello.java
@@ -12,13 +12,13 @@ public class Hello {
 
 
 
-    public static void main(String[] args) throws TypeMismatchException {
+    public static void main(String[] args) throws InputMismatchException {
         addAmounts(new Amount("TL",1000),new Amount("EURO",500));
 
     }
-    static Amount addAmounts(Amount a1, Amount a2) throws TypeMismatchException {
+    static Amount addAmounts(Amount a1, Amount a2) throws InputMismatchException {
         if(!a1.currency.equals(a2.currency)){
-            throw new TypeMismatchException("Currencies are not the same");
+            throw new InputMismatchException("Currencies are not the same");
         }
         return new Amount(a1.currency,a1.amount + a2.amount);
 
