21d9dab5bc51630253107959ed0bef90d49d9f25
anonimid <freelancetradertr@gmail.com>
2021-07-12 19:26:16 +0300

    added a new method

--- src/test/java/com/cbt/day16_homework/Hello.java
+++ src/test/java/com/cbt/day16_homework/Hello.java
@@ -3,22 +3,26 @@ package com.cbt.day16_homework;
 import com.cbt.utilities.BrowserFactory;
 import com.sun.corba.se.impl.io.TypeMismatchException;
 import net.sourceforge.tess4j.Tesseract;
-import org.openqa.selenium.TakesScreenshot;
+
 import org.openqa.selenium.WebDriver;
 
 import java.util.InputMismatchException;
 
 public class Hello {
 
-
-
-    public static void main(String[] args) throws InputMismatchException {
-        addAmounts(new Amount("TL",1000),new Amount("EURO",500));
-
+    public static Object get(Object[] array,int index){
+        return array[index];
     }
-    static Amount addAmounts(Amount a1, Amount a2) throws InputMismatchException {
+
+
+    public static void main(String[] args) throws TypeMismatchException {
+        Amount amount = addAmounts(new Amount("TL",1000),new Amount("EURO",500));
+        System.out.println(amount.amount);
+        System.out.println(amount.currency);
+    }
+    static Amount addAmounts(Amount a1, Amount a2) throws TypeMismatchException {
         if(!a1.currency.equals(a2.currency)){
-            throw new InputMismatchException("Currencies are not the same");
+            throw new TypeMismatchException("Currencies are not the same");
         }
         return new Amount(a1.currency,a1.amount + a2.amount);
 
