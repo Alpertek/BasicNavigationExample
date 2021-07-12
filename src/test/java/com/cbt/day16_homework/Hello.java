a5baacff4a8b5f3902c186f348bd4cb0fee89912
anonimid <freelancetradertr@gmail.com>
2021-07-12 19:04:12 +0300

    Hello.java is changed

--- src/test/java/com/cbt/day16_homework/Hello.java
+++ src/test/java/com/cbt/day16_homework/Hello.java
@@ -1,6 +1,7 @@
 package com.cbt.day16_homework;
 
 import com.cbt.utilities.BrowserFactory;
+import com.sun.corba.se.impl.io.TypeMismatchException;
 import net.sourceforge.tess4j.Tesseract;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -11,17 +12,24 @@ public class Hello {
 
 
 
-    public static void main(String[] args) {
-//        WebDriver driver = BrowserFactory.getDriver("chrome");
-//        driver.get("https://mvnrepository.com/artifact/com.aspose/aspose-cloud-storage");
-//        TakesScreenshot ss = (TakesScreenshot) driver;
-        try{
-            System.out.println(1/0);
-        }finally{
-            System.out.println("bye;");
+    public static void main(String[] args) throws TypeMismatchException {
+        addAmounts(new Amount("TL",1000),new Amount("EURO",500));
+
+    }
+    static Amount addAmounts(Amount a1, Amount a2) throws TypeMismatchException {
+        if(!a1.currency.equals(a2.currency)){
+            throw new TypeMismatchException("Currencies are not the same");
         }
+        return new Amount(a1.currency,a1.amount + a2.amount);
 
-
+    }
+    static class Amount{
+        String currency;
+        int amount;
+        Amount(String currency,int amount){
+            this.currency = currency;
+            this.amount = amount;
+        }
     }
 
 }
