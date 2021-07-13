15ab8b650c850939270a3d1b5813db243573aa3a
anonimid <freelancetradertr@gmail.com>
2021-07-13 11:07:58 +0300

    code blocks are studied

--- src/test/java/com/cbt/tests/Test.java
+++ src/test/java/com/cbt/tests/Test.java
@@ -1,8 +1,25 @@
 package com.cbt.tests;
 
 public class Test {
+    int count = setCount(10);
+    {
+        count = setCount(5);
+    }
+    Test(){
 
+    }
 
+    public int setCount(int count){
+        System.out.println("Test.count = " + this.count);
+        this.count = count;
+        System.out.println("Test.count = " + this.count);
+        return count;
+    }
+}
+class TestTest {
+    public static void main(String[] args) {
+        Test test = new Test();
+    }
 }
 
 
